fa85364e8554a79269971118f3dcce21844a3038
Clockware <Clockware@users.noreply.github.com>
2014-12-16 22:39:51 +0600

    matrix_A def

--- lab3.py
+++ lab3.py
@@ -17,6 +17,15 @@ def parse_matrix(lines):
 
 
 if __name__ == '__main__':
+
+    N = int(raw_input())
+    M = 21 - N
+    A = 0.1 * M + 0.01 * N
+    B = 0.2 * M + 0.02 * N + 0.01 * M * N + 0.001 * N * N
+    matrix_A = [[1.2345, 3.1415, 1], [2.3456, 5.9690, 0], [3.4567, 2.1828, (2 + 0.1 * N)]]
+    vector_b = [[7.5175 + A], [14.2836], [7.8223 + B]]
+
+
     getcontext().rounding = ROUND_DOWN
     print 'Matrix:'
     A = parse_matrix(iter(raw_input, ''))
