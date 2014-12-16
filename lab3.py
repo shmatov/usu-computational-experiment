27751e9087fd826ea91d16c46a72ed86e8f18482
Roman Shmatov <romanshmatov@gmail.com>
2014-12-16 17:48:57 +0500

    round down decimals

--- lab3.py
+++ lab3.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python
+import decimal
+from decimal import Decimal
 import math
+
 import numpy
 
+
+def solve():
+    pass
+
+
+if __name__ == '__main__':
+    decimal.getcontext().rounding = decimal.ROUND_DOWN
+    for k in (2, 4, 6):
+        decimal.getcontext().prec = k
+        solve()
 
