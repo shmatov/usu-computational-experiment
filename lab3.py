19983ac58328fbdd9d7c2b7e00282a9d14e1876f
Roman Shmatov <romanshmatov@gmail.com>
2014-12-17 19:30:08 +0500

    lu decomposition

--- lab3.py
+++ lab3.py
@@ -2,93 +2,159 @@
 
 from copy import deepcopy
 from decimal import Decimal, ROUND_DOWN
-from functools import wraps
 import math
 
 
-def simple_equation_solver(matrix, vector):
-    new_vector = [None] * 3
-    new_vector[2] = vector[2][0] / matrix[2][2]
-    new_vector[1] = (vector[1][0] - matrix[1][2] * new_vector[2]) / matrix[1][1]
-    new_vector[0] = (vector[0][0] - matrix[0][1] * new_vector[1] - matrix[0][2] * new_vector[2]) / matrix[0][0]
-    return new_vector
+WIDTH = 80
 
 
-def multiply(X, Y):
-    return [
-        [
-            sum(a * b for a, b in zip(X_row, Y_col))
-            for Y_col in zip(*Y)
-        ]
-        for X_row in X
-    ]
+class Matrix(object):
+    def __init__(self, matrix):
+        self._matrix = matrix
+
+    def __iter__(self):
+        return iter(self._matrix)
+
+    def __mul__(self, other):
+        if not isinstance(other, type(self)):
+            raise NotImplementedError(
+                '{} * {}'.format(type(self).__name__, type(other).__name__)
+            )
+
+        return Matrix([
+            [
+                sum(a * b for a, b in zip(self_row, other_col))
+                for other_col in zip(*other)
+            ]
+            for self_row in self
+        ])
+
+    def __getitem__(self, index):
+        return self._matrix[index]
+
+    def __setitem__(self, index, value):
+        self._matrix[index] = value
+        return value
+
+    def __repr__(self):
+        max_lengths = [max(len(str(x)) for x in col) for col in zip(*self)]
+        return '\n'.join([
+            '  '.join([
+                str(x).rjust(l) for x, l in zip(row, max_lengths)
+            ])
+            for row in self
+        ])
+
+    def __len__(self):
+        return len(self._matrix)
+
+    @property
+    def rows(self):
+        return len(self._matrix)
+
+    @property
+    def cols(self):
+        return len(self[0])
+
+    def map(self, func):
+        return Matrix([[func(x) for x in row] for row in self])
+
+    def adj(self):
+        pass
+
+    def lu_decomposition(self):
+        n = self.rows
+
+        l = self.map(lambda x: x - x)
+        u = self.map(lambda x: x - x)
+
+        for j in xrange(n):
+            l[j][j] = abs(self[j][j] / self[j][j])
+
+            for i in xrange(j+1):
+                s1 = sum(u[k][j] * l[i][k] for k in xrange(i))
+                u[i][j] = self[i][j] - s1
+
+            for i in xrange(j, n):
+                s2 = sum(u[k][j] * l[i][k] for k in xrange(j))
+                l[i][j] = (self[i][j] - s2) / u[j][j]
+
+        return l, u
 
 
-def patch_decimal(precision):
-    decimal_precision = Decimal('.' + '0' * precision)
+class Num(Decimal):
+    precision = 2
 
-    def truncate_wrapper(method):
-        @wraps(method)
-        def wrapper(*args, **kwargs):
-            result = method(*args, **kwargs)
-            if isinstance(result, Decimal):
-                return result.quantize(decimal_precision, rounding=ROUND_DOWN)
-            return result
+    def __new__(cls, value):
+        if isinstance(value, float):
+            value = ('{:.' + str(cls.precision) + 'f}').format(value)
 
-        return wrapper
+        return super(Num, cls).__new__(cls, value)
 
-    methods = ['__add__', '__sub__', '__mul__', '__floordiv__', '__mod__',
-               '__divmod__', '__pow__', '__div__']
-    for method in methods:
-        setattr(Decimal, method, truncate_wrapper(getattr(Decimal, method)))
+    def _quantize(self, value):
+        if isinstance(value, Decimal):
+            decimal_precision = Decimal('.' + '0' * self.precision)
+            return Num(value.quantize(decimal_precision, rounding=ROUND_DOWN))
+        return value
 
-    def new_wrapper(wrapped_func):
-        def wrapper(cls, value, *args, **kwargs):
-            if isinstance(value, float):
-                value = ('{:.' + str(precision) + 'f}').format(value)
-            return wrapped_func(cls, value, *args, **kwargs)
+    def __mul__(self, *args, **kwargs):
+        return self._quantize(super(Num, self).__mul__(*args, **kwargs))
 
-        return staticmethod(wrapper)
+    def __add__(self, *args, **kwargs):
+        return self._quantize(super(Num, self).__add__(*args, **kwargs))
 
-    Decimal.__new__ = new_wrapper(Decimal.__new__)
+    def __sub__(self, *args, **kwargs):
+        return self._quantize(super(Num, self).__sub__(*args, **kwargs))
 
+    def __div__(self, *args, **kwargs):
+        return self._quantize(super(Num, self).__div__(*args, **kwargs))
 
-def to_decimal_matrix(matrix):
-    return [
-        [Decimal(x) for x in row]
-        for row in matrix
-    ]
+    def __mod__(self, *args, **kwargs):
+        return self._quantize(super(Num, self).__mod__(*args, **kwargs))
+
+    def __pow__(self, *args, **kwargs):
+        return self._quantize(super(Num, self).__pow__(*args, **kwargs))
+
+    def __repr__(self):
+        return 'Num({})'.format(self)
 
 
 def generate_matrix_and_vector_and_answer(n):
     m = 21 - n
     a = 0.1 * m + 0.01 * n
     b = 0.2 * m + 0.02 * n + 0.01 * m * n + 0.001 * n * n
-    matrix = [
+    matrix = Matrix([
         [1.2345, 3.1415, 1],
         [2.3456, 5.9690, 0],
         [3.4567, 2.1828, (2 + 0.1 * n)]
-    ]
-    vector = [[7.5175 + a], [14.2836], [7.8223 + b]]
-    answer = [[1], [2], [0.1 * m + 0.01 * n]]
-    return map(to_decimal_matrix, (matrix, vector, answer))
+    ])
+    vector = Matrix([
+        [7.5175 + a],
+        [14.2836],
+        [7.8223 + b]
+    ])
+    answer = Matrix([
+        [1],
+        [2],
+        [0.1 * m + 0.01 * n]
+    ])
+    return matrix.map(Num), vector.map(Num), answer
 
 
-def simple_equation_solver(matrix, vector):
-    new_vector = [None] * 3
-    new_vector[2] = vector[2][0] / matrix[2][2]
-    new_vector[1] = (vector[1][0] - matrix[1][2] * new_vector[2]) / matrix[1][1]
-    new_vector[0] = (vector[0][0] - matrix[0][1] * new_vector[1] - matrix[0][2] * new_vector[2]) / matrix[0][0]
-    for x in range(0, 3):
-        new_vector[x] = [new_vector[x]]
-    return new_vector
+def simple_equation_solver(mx, vec):
+    nvec = [None] * 3
+    nvec[2] = vec[2][0] / mx[2][2]
+    nvec[1] = (vec[1][0] - mx[1][2] * nvec[2]) / mx[1][1]
+    nvec[0] = (vec[0][0] - mx[0][1] * nvec[1] - mx[0][2] * nvec[2]) / mx[0][0]
+    return Matrix([[x] for x in nvec])
 
 
 def gauss_transformation(matrix, vector):
     matrix = deepcopy(matrix)
+
     vector = deepcopy(vector)
     for column in range(0, 2):
-        maximum = column;
+        maximum = column
         for line in range(column + 1, 3):
             if abs(matrix[maximum][column]) < abs(matrix[line][column]):
                 maximum = line
@@ -100,38 +166,69 @@ def gauss_transformation(matrix, vector):
                 k = matrix[column][column] / matrix[line][column]
                 vector[line][0] *= k
                 vector[line][0] -= vector[column][0]
-                matrix[line][column] = 0;
+                matrix[line][column] = 0
                 for rest_index in range(column + 1, 3):
                     matrix[line][rest_index] *= k
                     matrix[line][rest_index] -= matrix[column][rest_index]
     return matrix, vector
 
 
-def vector_distance(vector1, vector2):
-    summary = 0.0
-    for x in range(0, 3):
-        diff = float(vector1[x][0] - vector2[x][0])
-        summary += diff * diff
-    return math.sqrt(summary)
+def vector_distance(v1, v2):
+    squares = map(lambda (x, y): float(x[0] - y[0]) ** 2, zip(v1, v2))
+    return math.sqrt(sum(squares))
+
+
+def show(name, matrix):
+    print ''
+    print name
+    print repr(matrix)
+
+
+def solver_1(mx, vec):
+    print ''
+    print 'Method 1'.center(WIDTH)
+    print ''
+    print '-' * WIDTH
+
+    print 'LU decomposition.'
+    l, u = mx.lu_decomposition()
+    show('L', l)
+    show('U', u)
+    show('LU', l * u)
+    print '=' * WIDTH
+
+
+def solver_2(mx, vec):
+    print ''
+    print 'Method 2'.center(WIDTH)
+    print ''
+    print '-' * WIDTH
+
+    m_gauss, v_gauss = gauss_transformation(mx, vec)
+    print 'After gauss transformation.'
+    show('Matrix:', m_gauss)
+    show('Vector:', v_gauss)
+    print '-' * WIDTH
+
+    calc_ans = simple_equation_solver(m_gauss, v_gauss)
+    print 'Answer after calc:\n'
+    print repr(calc_ans)
+    print '-' * WIDTH
+
+    print 'Diff between real and calculated: {}'.format(
+        vector_distance(ans, calc_ans.map(float))
+    )
+    print '=' * WIDTH
 
 
 if __name__ == '__main__':
-    patch_decimal(2)
-    n = int(raw_input())
-    m, v, ans = generate_matrix_and_vector_and_answer(n)
-    print "Task for N"
-    print repr(m)
-    print repr(v)
-    print "Answer for N"
-    print repr(ans)
+    n = int(raw_input('Solve task for N = '))
+    mx, vec, ans = generate_matrix_and_vector_and_answer(n)
 
-    # !solver2
-    m_gauss, v_gauss = gauss_transformation(m, v)
-    print "After gauss transformation:"
-    print repr(m_gauss)
-    print repr(v_gauss)
-    calc_ans = simple_equation_solver(m_gauss, v_gauss)
-    print "Ans after calc:"
-    print repr(calc_ans)
-    print "Diff between real and calculated:"
-    print vector_distance(ans, calc_ans)
+    show('Matrix:', mx)
+    show('Vector:', vec)
+    show('Answer:', ans)
+    print '=' * WIDTH
+
+    solver_1(mx, vec)
+    solver_2(mx, vec)
