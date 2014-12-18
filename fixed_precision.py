0f608bbd2f1eefb596e319b96529c80ea636b702
Roman Shmatov <romanshmatov@gmail.com>
2014-12-18 17:54:50 +0500

    oh lol

---
+++ fixed_precision.py
@@ -0,0 +1,38 @@
+from decimal import Decimal, ROUND_DOWN
+
+
+class Num(Decimal):
+    precision = 10
+
+    def __new__(cls, value):
+        if isinstance(value, float):
+            value = ('{:.' + str(cls.precision) + 'f}').format(value)
+
+        return super(Num, cls).__new__(cls, value)
+
+    def _quantize(self, value):
+        if isinstance(value, Decimal):
+            decimal_precision = Decimal('.' + '0' * self.precision)
+            return Num(value.quantize(decimal_precision, rounding=ROUND_DOWN))
+        return value
+
+    def __mul__(self, *args, **kwargs):
+        return self._quantize(super(Num, self).__mul__(*args, **kwargs))
+
+    def __add__(self, *args, **kwargs):
+        return self._quantize(super(Num, self).__add__(*args, **kwargs))
+
+    def __sub__(self, *args, **kwargs):
+        return self._quantize(super(Num, self).__sub__(*args, **kwargs))
+
+    def __div__(self, *args, **kwargs):
+        return self._quantize(super(Num, self).__div__(*args, **kwargs))
+
+    def __mod__(self, *args, **kwargs):
+        return self._quantize(super(Num, self).__mod__(*args, **kwargs))
+
+    def __pow__(self, *args, **kwargs):
+        return self._quantize(super(Num, self).__pow__(*args, **kwargs))
+
+    def __repr__(self):
+        return 'Num({})'.format(self)
