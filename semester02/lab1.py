9aecc202b27af0adc81b5c3a0f070c2923ae3220
Clockware <Clockware@users.noreply.github.com>
2015-03-22 21:35:23 +0600

    moar definitions

--- semester02/lab1.py
+++ semester02/lab1.py
@@ -4,9 +4,36 @@ import math
 """
 
 decomposition:
-1)...
-2)...
-..
+for lab 1.1:
+1)Interface: method(Function function, double step) -> double
+Srednie_pryamougolniki
+Trapecii
+Simpson
+
+2)Rn[f] - ? I forgot how to count that
+Rerror(Function function, double step) -> double
+
+3)LaTeX: tags for tables
+
+
+for lab 1.2:
+<pick some N from imagination> [I recommend 15]
+from i = 1 to N 
+	step = 1/i;
+	using 1) from 1.1 use method(func, step)
+using 3) from 1.1 make table
+4)Make N decart graphs with point to point lines, where point is (x, f(x)), x from each step;
+
+
+for lab 1.3:
+use analytics to spent error and make integral a finite one.
+N = 2;
+while(abs(prev_integral - act_intergral) < epsilon)
+	N = N * 2;
+	prev_integral = act_intergral
+	act_intergral = using any method 1) from 1.1 with step 1/N;
+print act_intergral
+
 
 """
 
