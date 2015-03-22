275116eeecba13c8ce48bddcc8e90be1cf55c53e
Clockware <Clockware@users.noreply.github.com>
2015-03-22 22:04:38 +0600

    some more boilerplate

--- semester02/lab1.py
+++ semester02/lab1.py
@@ -5,7 +5,7 @@ import math
 
 decomposition:
 for lab 1.1:
-1)Interface: method(Function function, double step) -> double
+1)Interface: method(Function function, double from, double to, double step) -> double
 Srednie_pryamougolniki
 Trapecii
 Simpson
@@ -37,5 +37,72 @@ print act_intergral
 
 """
 
+B = 1 #random const from my head
+
+def f1(x):
+	return math.sqrt(1 + x + x*x + B)
+
+def f2(x):
+	return 1/(1 + x*x)
+
+def f3(x):
+	return math.arctg(x) / (1 + x*x*x)
+
+
+
+def rectangle_method(function, left, right, step):
+	return (left + right) / 2 #boilerplate
+
+def trapezoidal_rule(function, left, right, step):
+	return (left + right) / 2 #boilerplate
+
+def simpson(function, left, right, step):
+	return (left + right) / 2 #boilerplate
+
+
+
+def step_for_interval(left, right, n):
+	return (right - left) / n
+
+def compute_intergral_with_eps(method, function, left, right, eps):
+	N = 3
+	prev_integral = method(function, left, right, step_for_interval(left, right, N - 1))
+	current_integral = method(function, left, right, step_for_interval(left, right, N)
+	while(math.abs(prev_integral - current_integral) < eps):
+		N = N + 1
+		prev_integral = current_integral
+		current_integral = method(function, left, right, step_for_interval(left, right, N)
+	return current_integral
+
+
+
+
+def solve_task01:
+	left = 0
+	right = 1
+	print rectangle_method(*f1, left, right, 0.1)
+	print rectangle_method(*f1, left, right, 0.05)
+	print trapezoidal_rule(*f1, left, right, 0.1)
+	print trapezoidal_rule(*f1, left, right, 0.05)
+	print simpson(*f1, left, right, 0.1)
+	print simpson(*f1, left, right, 0.05)
+	# Rn - ?
+
+def solve_task02:
+	pass
+
+
+def solve_task03:
+	left = 0
+	right = 13
+	eps = 0.005
+	method = *trapezoidal_rule
+	print compute_intergral_with_eps(method, *f3, left, right, eps)
+
+
 if __name__ == '__main__':
-    print 1
+    # 1.3
+    A = 13
+    
+    
+
