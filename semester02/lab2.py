1111a107c2790bca7c171b1d9568d78ce9730c39
Clockware <Clockware@users.noreply.github.com>
2015-04-25 12:07:53 +0500

    Refactoring after all

--- semester02/lab2.py
+++ semester02/lab2.py
@@ -3,59 +3,117 @@ import math  # noqa
 import matplotlib.pyplot as plt
 
 
-# TASK INITIAL
+# INITIAL DATA
+
+# COMMON
 
 initial_a = 0.0
 initial_b = 1.0
-
-initial_y0 = 0.5
-# initial_y0 = 0.1
-# initial_y0 = 0.1
-
-variant = 0.4
+variant = 4
 
 
-def initial_dy(x, y):
-    # return 30 * y * (x - 0.2) * (x - 0.7)
-    # return 50 * y * (x - 0.6) * (x - 0.85)
-    return -20 * y ** 2 * (x - variant)
+# TASK A
+
+task_a_initial_y0 = 0.1
+
+def task_a_initial_dy(x, y):
+    return 30 * y * (x - 0.2) * (x - 0.7)
+
+def task_a_initial_ddy(x, y):
+    return 30 * (task_a_initial_dy(x, y) * (x - 0.2) * (x - 0.7) + y * (2 * x - 0.9))
+
+def task_a_initial_dddy(x, y):
+    return 30 * (task_a_initial_ddy(x, y) * (x - 0.2) * (x - 0.7) + 2 * task_a_initial_dy(x, y) * (2 * x - 0.9) + 2 * y)
+
+def task_a_initial_euler_backwards(x, y, step):
+    return y / (1 - 30 * step * (x + step - 0.2) * (x + step - 0.7))
+
+def task_a_initial_solution_of_y(x):
+    return math.exp(x * (10 * x ** 2 - 13.5 * x + 4.2)) / 10
+
+TASK_A = {
+    'a': initial_a,
+    'b': initial_b,
+    'y0': task_a_initial_y0,
+    'dy': task_a_initial_dy,
+    'ddy': task_a_initial_ddy,
+    'dddy': task_a_initial_dddy,
+    'euler_backwards': task_a_initial_euler_backwards,
+    'solution_of_y': task_a_initial_solution_of_y
+}
+
+# TASK B
+
+task_b_initial_y0 = 0.1
+
+def task_b_initial_dy(x, y):
+    return 50 * y * (x - 0.6) * (x - 0.85)
+
+def task_b_initial_ddy(x, y):
+    return 50 * (
+        task_b_initial_dy(x, y) * (x - 0.6) * (x - 0.85) +
+        y * (2 * x - 1.45)
+    )
+
+def task_b_initial_dddy(x, y):
+    return 50 * (
+        task_b_initial_ddy(x, y) * (x - 0.6) * (x - 0.85) +
+        2 * task_b_initial_dy(x, y) * (2 * x - 1.45) +
+        2 * y
+    )
+
+def task_b_initial_euler_backwards(x, y, step):
+    return y / (1 - 50 * step * (x + step - 0.6) * (x + step - 0.85))
+
+def task_b_initial_solution_of_y(x):
+    return math.exp(x * (50 / 3. * x ** 2 - 36.25 * x + 25.5)) / 10
+
+TASK_B = {
+    'a': initial_a,
+    'b': initial_b,
+    'y0': task_b_initial_y0,
+    'dy': task_b_initial_dy,
+    'ddy': task_b_initial_ddy,
+    'dddy': task_b_initial_dddy,
+    'euler_backwards': task_b_initial_euler_backwards,
+    'solution_of_y': task_b_initial_solution_of_y
+}
 
 
-def initial_ddy(x, y):
-    # return 30 * (initial_dy(x, y) * (x - 0.2) * (x - 0.7) + y * (2 * x - 0.9))
-    # return 50 * (
-    #     initial_dy(x, y) * (x - 0.6) * (x - 0.85) +
-    #     y * (2 * x - 1.45)
-    # )
-    return -20 * (2 * initial_dy(x, y) * (x - variant) + y ** 2)
+# TASK C
 
+task_c_initial_y0 = 0.5
 
-def initial_dddy(x, y):
-    # return 30 * (initial_ddy(x, y) * (x - 0.2) * (x - 0.7) + 2 * initial_dy(x, y) * (2 * x - 0.9) + 2 * y)
-    # return 50 * (
-    #     initial_ddy(x, y) * (x - 0.6) * (x - 0.85) +
-    #     2 * initial_dy(x, y) * (2 * x - 1.45) +
-    #     2 * y
-    # )
+def task_c_initial_dy(x, y):
+    return -20 * y ** 2 * (x - (variant * 0.1))
+
+def task_c_initial_ddy(x, y):
+    return -20 * (2 * task_c_initial_dy(x, y) * (x - (variant * 0.1)) + y ** 2)
+
+def task_c_initial_dddy(x, y):
     return -20 * (
-        2 * initial_ddy(x, y) * (x - variant) + 2 * initial_dy(x, y) + 2 * y
+        2 * task_c_initial_ddy(x, y) * (x - (variant * 0.1)) + 2 * task_c_initial_dy(x, y) + 2 * y
     )
 
-
-def initial_euler_backwards(x, y, step):
-    # return y / (1 - 30 * step * (x + step - 0.2) * (x + step - 0.7))
-    # return y / (1 - 50 * step * (x + step - 0.6) * (x + step - 0.85))
-    # return 1
+def task_c_initial_euler_backwards(x, y, step):
     return (
-        (-1 + math.sqrt(1 + 80 * step * (x + step - variant))) /
-        (40 * step * (x + step - variant))
+        (-1 + math.sqrt(1 + 80 * step * (x + step - (variant * 0.1)) * y)) /
+        (40 * step * (x + step - (variant * 0.1)))
     )
 
+def task_c_initial_solution_of_y(x):
+    return - 0.05 / ((variant * 0.1) * x - 0.5 * x ** 2 - 0.1)
 
-def initial_solution_of_y(x):
-    # return math.exp(x * (10 * x ** 2 - 13.5 * x + 4.2)) / 10
-    # return math.exp(x * (50 / 3. * x ** 2 - 36.25 * x + 25.5)) / 10
-    return - 0.05 / (variant * x - 0.5 * x ** 2 - 0.1)
+TASK_C = {
+    'a': initial_a,
+    'b': initial_b,
+    'y0': task_c_initial_y0,
+    'dy': task_c_initial_dy,
+    'ddy': task_c_initial_ddy,
+    'dddy': task_c_initial_dddy,
+    'euler_backwards': task_c_initial_euler_backwards,
+    'solution_of_y': task_c_initial_solution_of_y
+}
 
 
 # ONE STEP CALCULATIONS
@@ -69,8 +127,8 @@ def one_step_recalculation_euler(x, y, step, dy, **_):
     return y + step * (dy(x, y) + dy(x + step, y_new)) / 2
 
 
-def one_step_implicit_euler(x, y, step, dy, dy_euler_backwards, **_):
-    return dy_euler_backwards(x, y, step)
+def one_step_implicit_euler(x, y, step, dy, euler_backwards, **_):
+    return euler_backwards(x, y, step)
 
 
 def one_step_cauchy(x, y, step, dy, **_):
@@ -115,53 +173,24 @@ def one_step_analytic_solution(x, y, step, dy, solution_of_y, **_):
 
 # TIED UP ONE STEP METHODS AND ACCELERATORS
 
-def EXPLICIT_EULER():
-    return (one_step_explicit_euler, 0, None)
-
-
-def RECALCULATION_EULER():
-    return (one_step_recalculation_euler, 0, None)
-
-
-def IMPLICIT_EULER():
-    return (one_step_implicit_euler, 0, None)
-
-
-def CAUCHY():
-    return (one_step_cauchy, 0, None)
-
-
-def TAYLOR_DEGREE_2():
-    return (one_step_taylor_degree_2, 0, None)
-
-
-def TAYLOR_DEGREE_3():
-    return (one_step_taylor_degree_3, 0, None)
-
-
-def TAYLOR_DEGREE_4():
-    return (one_step_taylor_degree_4, 0, None)
-
-
-def ADAMS_2_STEPS():
-    return (one_step_adams_two_steps_usage, 1, one_step_recalculation_euler)
-
-
-def ADAMS_3_STEPS():
-    return (one_step_adams_three_steps_usage, 2, one_step_recalculation_euler)
-
-
-def RUNGE_KUTTA():
-    return (one_step_runge_kutta, 0, None)
-
-
-def ANALYTIC_SOLUTION():
-    return (one_step_analytic_solution, 0, None)
+methods = {
+    'EXPLICIT_EULER': (one_step_explicit_euler, 0, None),
+    'RECALCULATION_EULER': (one_step_recalculation_euler, 0, None),
+    'IMPLICIT_EULER': (one_step_implicit_euler, 0, None),
+    'CAUCHY': (one_step_cauchy, 0, None),
+    'TAYLOR_DEGREE_2': (one_step_taylor_degree_2, 0, None),
+    'TAYLOR_DEGREE_3': (one_step_taylor_degree_3, 0, None),
+    'TAYLOR_DEGREE_4': (one_step_taylor_degree_4, 0, None),
+    'ADAMS_2_STEPS': (one_step_adams_two_steps_usage, 1, one_step_recalculation_euler),
+    'ADAMS_3_STEPS': (one_step_adams_two_steps_usage, 2, one_step_recalculation_euler),
+    'RUNGE_KUTTA': (one_step_runge_kutta, 0, None),
+    'ANALYTIC_SOLUTION': (one_step_analytic_solution, 0, None)
+}
 
 
 # SOLVER
 
-def common_solver(method_description, a, b, steps_count, y0, dy, ddy, dddy, dy_euler_backwards, solution_of_y):
+def common_solver(method, steps_count, a, b, y0, dy, ddy, dddy, euler_backwards, solution_of_y):
     assert(isinstance(a, float))
     assert(isinstance(b, float))
     assert(a < b)
@@ -173,7 +202,7 @@ def common_solver(method_description, a, b, steps_count, y0, dy, ddy, dddy, dy_e
     pair_list = [(x, y)]
     step = (b - a) / steps_count
 
-    major_method, precalc_steps_count, precalc_method = method_description
+    major_method, precalc_steps_count, precalc_method = method
 
     for _ in range(precalc_steps_count):
         x, y = x + step, precalc_method(x, y, step, dy)
@@ -185,7 +214,7 @@ def common_solver(method_description, a, b, steps_count, y0, dy, ddy, dddy, dy_e
             'dddy': dddy,
             'prev1': pair_list[-2] if len(pair_list) > 1 else None,
             'prev2': pair_list[-3] if len(pair_list) > 2 else None,
-            'dy_euler_backwards': dy_euler_backwards,
+            'euler_backwards': euler_backwards,
             'solution_of_y': solution_of_y
         }
         x, y = x + step, major_method(x, y, step, dy, **additional_method_kwargs)
@@ -195,21 +224,6 @@ def common_solver(method_description, a, b, steps_count, y0, dy, ddy, dddy, dy_e
 
 
 # MAIN
-
-def get_plot_list_for_method(method, n):
-    return common_solver(
-        method(),
-        initial_a,
-        initial_b,
-        n,
-        initial_y0,
-        initial_dy,
-        initial_ddy,
-        initial_dddy,
-        initial_euler_backwards,
-        initial_solution_of_y
-    )
-
 
 def plot(methods, steps):
     plt.figure()
@@ -230,27 +244,14 @@ def plot(methods, steps):
 
 
 def compute(steps):
-    methods = [
-        EXPLICIT_EULER,
-        RECALCULATION_EULER,
-        IMPLICIT_EULER,
-        CAUCHY,
-        TAYLOR_DEGREE_2,
-        TAYLOR_DEGREE_3,
-        TAYLOR_DEGREE_4,
-        ADAMS_2_STEPS,
-        ADAMS_3_STEPS,
-        RUNGE_KUTTA,
-        ANALYTIC_SOLUTION
-    ]
-
     data = {}
-    for method in methods:
-        method_name = method.func_name
-        points = get_plot_list_for_method(
-            method, 10000 if method == ANALYTIC_SOLUTION else steps
+    for name, method in methods.iteritems():
+        points = common_solver(
+            method,
+            steps if name != 'ANALYTIC_SOLUTION' else 10000,
+            **TASK_C
         )
-        data[method_name] = points
+        data[name] = points
     plot(data, steps)
 
 
